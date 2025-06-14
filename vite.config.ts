8f0856bc43ff6b097d2f7508013038bfa25b6d3c
yaobo <15170970773@163.com>
2025-06-14 20:18:13 +0800

    配置了，代码压缩，代码分包，图片压缩

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,50 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
+import viteCompression from "vite-plugin-compression";
+import { visualizer } from "rollup-plugin-visualizer";
+import viteImagemin from "vite-plugin-imagemin";
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
+  plugins: [
+    react(),
+    // 压缩插件，打包时启用
+    viteCompression({
+      threshold: 10240,
+      algorithm: "gzip",
+      ext: ".gz",
+      deleteOriginFile: true,
+    }),
+    // 打包分析插件
+    visualizer({ open: true }),
+    // 图片压缩插件
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
+  resolve: {},
   server: {
     port: 8000,
     // 设置服务器主机名
@@ -28,6 +64,15 @@ export default defineConfig({
       "/tasks": {
         target: "http://localhost:3001",
         changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "js/[name]-[hash].js",
+        entryFileNames: "js/[name]-[hash].js",
+        assetFileNames: "[ext]/[name]-[hash].[ext]",
       },
     },
   },
